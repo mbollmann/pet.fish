bb6ee47d0bdf8048ea60633ed22097358deb68e6
ffact <114359275+ffact@users.noreply.github.com>
2024-10-01 07:47:13 +0000

    Multiline fix for __pet_preview.fish

--- functions/__pet_preview.fish
+++ functions/__pet_preview.fish
@@ -1,4 +1,5 @@
 function __pet_preview --description "Takes a snippet formatted by `pet search` and turns it into a preview pane for fzf."
+    set -l tags (echo (echo $argv | sed 's/\\\\n/\n/g')[-1] | sed 's/[^#]*#/#/')
     # Description
     set_color blue --bold
     echo $argv | sed 's/^\[// ; s/\]: .*//'
@@ -6,14 +7,16 @@ function __pet_preview --description "Takes a snippet formatted by `pet search` 
     echo ""
     # Code (syntax-highlighted for fish)
     echo $argv | \
-        sed 's/.*]: // ; s/#.*//' | # extract code
+        sed 's/\\\\n/\n/g' |  # replace pet newlines with normal ones
+        sed '0,/\[[^]]*\]:/s///' | # delete bracketed description
+        sed "s/$tags//g" | # find and delete tags 
         sed 's/<\([^ =]\+\)\([^ ]*\)>/{$\U\1}/g' | # replace pet placeholders with variable names
         fish_indent --ansi
-    # Tags, if present
+    # Tags, if present  
     if string match -e -q "#" "$argv"
         echo ""
         # to be able to set tags with background color, loop over them individually
-        for tag in (echo $argv | sed 's/[^#]*#/#/' | string split " ")
+        for tag in $tags
             set_color magenta # -b white
             echo -n "$tag"
             set_color normal
